6779f382d2a8f9a1da1bd6ec4cbe2918c9a7b11b
Nacho <ignaciofeser@gmail.com>
2024-09-18 17:12:12 -0300

    commit 0.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("El mejor juego de los papus");
+        System.out.println("El mejor juego hermanoooo");
 
     }
 }
