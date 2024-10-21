b655f2d8562de5c661a64c7b327e51b2e5ccd8f7
Nacho <ignaciofeser@gmail.com>
2024-10-21 17:41:06 -0300

    Creando branch de accion por texto

--- src/main/java/org/example/Combate.java
+++ src/main/java/org/example/Combate.java
@@ -7,6 +7,78 @@ import java.util.Scanner;
 
 public class Combate {
 
+    public static void combate(GameState estado, Npc enemigo) {
+        Scanner scanner = new Scanner(System.in);
+        MainCharacter jugador = estado.getPersonajeActual();
+        Random random = new Random();
+        System.out.println("¡Prepárate para pelear!");
+
+        while (jugador.getSalud() > 0 && enemigo.getSalud() > 0) {
+            estado.mostrarEstado();
+
+            System.out.println("Es tu turno. Escribe una acción (por ejemplo, 'atacar al goblin' o 'usar habilidad especial'):");
+
+            String accion = scanner.nextLine().toLowerCase();  // Convertimos todo a minúsculas para facilitar el análisis
+
+            if (accion.contains("atacar")) {
+                int probabilidad = random.nextInt(100);
+                if (probabilidad < 75) {
+                    int danio = jugador.atacar(enemigo);
+                    System.out.println("Atacaste a " + enemigo.getNombre() + " y causaste " + danio + " de daño.");
+                } else {
+                    System.out.println("Has fallado el ataque.");
+                }
+            } else if (accion.contains("habilidad especial")) {
+                int probabilidad = random.nextInt(100);
+                if (probabilidad < 90) {
+                    if (jugador.usarHabilidadEspecial(enemigo)) {
+                        System.out.println("Usaste una habilidad especial en " + enemigo.getNombre() + ".");
+                    } else {
+                        System.out.println("Fallaste al usar tu habilidad especial.");
+                    }
+                }
+            } else {
+                System.out.println("Acción no válida o no reconocida.");
+            }
+
+            if (enemigo.getSalud() <= 0) {
+                System.out.println("Has derrotado a " + enemigo.getNombre() + "!");
+                estado.removerNpc(enemigo);
+                break;
+            }
+
+            System.out.println("Es el turno del enemigo.");
+            int probabilidad = random.nextInt(100);
+            if (probabilidad < 20) {
+                enemigo.habilidadEspecialNpc(jugador);
+                System.out.println("El enemigo " + enemigo.getNombre() + " usó su habilidad especial.");
+            } else {
+                int danioRecibido = enemigo.atacar(jugador);
+                System.out.println("El enemigo " + enemigo.getNombre() + " te atacó y causó " + danioRecibido + " de daño.");
+            }
+
+            if (jugador.getSalud() <= 0) {
+                System.out.println("Has sido derrotado. ¡Fin del juego!");
+                estado.setGameOver(true);
+                break;
+            }
+
+            jugador.regenerarMana();
+        }
+    }
+}
+
+
+/*
+package org.example;
+
+import org.example.Personajes.MainCharacter;
+import org.example.Personajes.Npc;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Combate {
+
     // Logica de combate
     public static void combate(GameState estado, Npc enemigo) {
         Scanner scanner = new Scanner(System.in);
@@ -84,3 +156,4 @@ public class Combate {
 
 
 
+*/
